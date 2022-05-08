84ef248060ae135cea631ead091ca8651c3a7ee5
koon <koon@koon.com>
2022-05-08 12:08:53 +0900

    02 NPE

--- src/main/kotlin/02/DatatypeUnsignedInteger.kt
+++ src/main/kotlin/02/DatatypeUnsignedInteger.kt
@@ -1,8 +1,8 @@
 package `02`
 
 fun main(args: Array<String>) {
-    val ubyte: UByte = 128u
+    val ubyte: UByte = 127u
     val ushort: UShort = 65000u
     val uint: UInt = 343u
-    val ulong: ULong = 3234156u
+    val ulong: ULong = 3234156uL
 }
---
+++ src/main/kotlin/02/NullAssign.kt
@@ -0,0 +1,12 @@
+package `02`
+
+fun main(args: Array<String>) {
+//    var str1: String = null
+    var str2: String? = null
+    var str3: String? = null
+
+//    println("str1: $str1")
+    println("str2: $str2, str2.lenth: ${str2?.length}")
+    println("str3: $str3, str3.lenth: ${str3!!.length}")
+
+}
